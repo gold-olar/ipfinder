72f4a6d27c4bb337716689d6bad3a0953b2193ea
Olamide Samuel <sam99kupo@gmail.com>
2019-08-02 01:01:53 +0100

    changed

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,29 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 require('dotenv').config;
-const api_key = process.env.API_KEY;
+const api_key = '85cd66a307c4ec9aa09de6292683cd24';
 const ip = require('ip');
 const address = ip.address();
 
+// const fetchData = "http://api.ipstack.com/196.27.128.5?access_key=85cd66a307c4ec9aa09de6292683cd24";
 
+const fetchData =`http://api.ipstack.com/${address}?access_key=${api_key}`
 
 
 router.get('/', function (req, res, next) {
-  // const fetchData =`http://api.ipstack.com/${address}?access_key=${api_key}`
 
-  const fetchData = "http://api.ipstack.com/196.27.128.5?access_key=85cd66a307c4ec9aa09de6292683cd24";
 
   async function getUser() {
     try {
       const response = await axios.get(fetchData);
-      console.log(response.data)
-      const { continent, country, region_name, latitude, longitude, ip } = response.data;
+      const { continent_name, country_name, region_name, latitude, longitude, ip } = response.data;
       res.json({
         ip: ip,
-        continent: continent,
-        country: country,
+        continent: continent_name,
+        country: country_name,
         state: region_name,
         latitude: latitude,
-        longitude: longitude
+        longitude: longitude,
       }).status(200)
 
     } catch (error) {
