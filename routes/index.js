40aae5cb9ea7e0079524771996b776f67593fa65
Olamide Samuel <sam99kupo@gmail.com>
2019-08-26 14:11:34 +0100

    done??

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,33 @@ require('dotenv').config;
 const api_key = '85cd66a307c4ec9aa09de6292683cd24';
 
 // Sample Ip Address can be...
-// '197.210.64.81';
+// const address = '197.210.64.81';
 
 
 
 
 router.get('/:address', function (req, res, next) {
   const {address} = req.params;
-  const fetchData =`http://api.ipstack.com/${address}?access_key=${api_key}`
-  async function getUser() {
-    try {
-    } catch (error) {
-      console.error(error);
+  const details = req.cookies.details;
+  const fetchData =`http://api.ipstack.com/${address}?access_key=${api_key}`;
+  if (details){
+    res.json(details).status(200);
+  }else{
+    async function getUser() {
+      try {
+
+        const response = await axios.get(fetchData);
+        const { continent_name, country_name, region_name, latitude, longitude, ip } = response.data;
+        const details =  { continent_name, country_name, region_name, latitude, longitude, ip }
+        res.cookie('details', details);
+        res.json(details).status(200);
+      } catch (error) {
+        console.error(error);
+      }
     }
+    getUser();
+
   }
-  getUser();
 
 });
 
